710e452b97dcc37c0763df4fd89c789890134e46
VarunChopra261 <87713922+VarunChopra261@users.noreply.github.com>
2024-09-03 11:57:41 +0530

    first commit

---
+++ my-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Navbar from '@/components/global/navbar'
+export default function Home() {
+  return (
+    <main>
+      <Navbar/>
+      <section className="h-screen w-full bg-neutral-950 rounded-md !overflow-visible relative flex flex-col items-center antialiased"></section>
+      
+    </main>
+  );
+}
