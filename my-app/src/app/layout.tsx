3cbf3be30f5e2e91ac671cef7a169313eb216348
vaibhavmannq <vaibhavmann.10@gmail.com>
2024-11-02 11:25:59 +0530

    Add clerk

---
+++ my-app/src/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import React from 'react';
+import { ThemeProvider } from "@/src/providers/theme-provider";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { User } from "lucide-react";
+
+const font = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "FlowSync",
+  description: "Automate Your Work with FlowSync",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider 
+    publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <html lang="en">
+        <body className={font.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <main>
+              {children}
+            </main>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
