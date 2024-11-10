68d9d3bf85f89f83369e1eb5b26eeee8af9bbcbf
vaibhavmannq <vaibhavmann.10@gmail.com>
2024-11-10 19:26:05 +0530

    Removed Uploadcare

--- my-app/src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
+++ my-app/src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
@@ -1,44 +1,46 @@
-'use client';
-import React, { useEffect, useRef } from 'react';
-import { FileUploaderRegular } from '@uploadcare/react-uploader/next';
-import '@uploadcare/react-uploader/core.css';
-import { useRouter } from 'next/navigation';
+// 'use client';
+// import React, { useEffect, useRef } from 'react';
+// import { FileUploaderRegular } from '@uploadcare/react-uploader/next';
+// import '@uploadcare/react-uploader/core.css';
+// import { useRouter } from 'next/navigation';
 
-type Props = {
-  onUpload: (e: string) => Promise<void>;
-};
+// type Props = {
+//   onUpload: (e: string) => Promise<void>;
+// };
 
-const UploadCareButton = ({ onUpload }: Props) => {
-  const router = useRouter();
-  const uploaderRef = useRef<HTMLDivElement | null>(null);
+// const UploadCareButton = ({ onUpload }: Props) => {
+//   const router = useRouter();
+//   const uploaderRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    const handleUpload = async (e: CustomEvent<{ cdnUrl: string }>) => {
-      await onUpload(e.detail.cdnUrl);
-      router.refresh();
-    };
+//   useEffect(() => {
+//     const handleUpload = async (e: CustomEvent<{ cdnUrl: string }>) => {
+//       await onUpload(e.detail.cdnUrl);
+//       router.refresh();
+//     };
 
-    const currentUploader = uploaderRef.current;
-    if (currentUploader) {
-      currentUploader.addEventListener('file-upload-success', (e) => handleUpload(e as CustomEvent<{ cdnUrl: string }>));
-    }
+//     const currentUploader = uploaderRef.current;
+//     if (currentUploader) {
+//       currentUploader.addEventListener('file-upload-success', (e) => handleUpload(e as CustomEvent<{ cdnUrl: string }>));
+//     }
 
-    return () => {
-      if (currentUploader) {
-        currentUploader.removeEventListener('file-upload-success', handleUpload as unknown as EventListener);
-      }
-    };
-  }, [onUpload, router]);
+//     return () => {
+//       if (currentUploader) {
+//         currentUploader.removeEventListener('file-upload-success', handleUpload as unknown as EventListener);
+//       }
+//     };
+//   }, [onUpload, router]);
 
-  return (
-    <div ref={uploaderRef}>
-      <FileUploaderRegular
-        sourceList="local, url, camera"
-        classNameUploader="uc-dark"
-        pubkey="e8a1ae37c26b62313900"
-      />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//     <div ref={uploaderRef}>
+//       <FileUploaderRegular
+//         sourceList="local, url, camera, dropbox, gdrive, onedrive"
+//         classNameUploader="uc-dark"
+//         pubkey="e8a1ae37c26b62313900"
+//       />
+//     </div>
+//     </div>
+//   );
+// };
 
-export default UploadCareButton;
+// export default UploadCareButton;
