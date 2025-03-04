5c82e32b9fee4662904c4b2a9a81854e48ce45f1
vaibhavmannq <vaibhavmann.10@gmail.com>
2025-03-04 11:31:54 +0530

    refactor EditorCanvasSidebar for improved state management and structure

--- my-app/src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas-sidebar.tsx
+++ my-app/src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas-sidebar.tsx
@@ -8,7 +8,6 @@ import { Separator } from '@/src/components/ui/separator'
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs'
 import React, { useEffect } from 'react'
 import EditorCanvasIconHelper from './editor-canvas-card-icon-helper'
-import { onDragStart } from '@/src/lib/editor-utils'
 import {
   Accordion,
   AccordionContent,
@@ -17,6 +16,12 @@ import {
 } from "@/components/ui/accordion"
 import RenderConnectionAccordion from './render-connection-accordion'
 import RenderOutputAccordion from './render-output-accordion'
+import { useFlowSyncStore } from '@/src/store'
+import {
+  // fetchBotSlackChannels,
+  onConnections,
+  onDragStart,
+} from '@/src/lib/editor-utils'
 
 
 type Props = {
@@ -27,54 +32,72 @@ type Props = {
 type Node = any;
 
 const EditorCanvasSidebar = ({ nodes }: Props) => {
-    const { state } = useEditor()
-    const { nodeConnection } = useNodeConnections()
-    return (
-      <aside>
-        <Tabs
-          defaultValue="actions"
-          className="h-screen overflow-scroll pb-24"
+  const { state } = useEditor()
+  const { nodeConnection } = useNodeConnections()
+  const { googleFile, setSlackChannels } = useFlowSyncStore()
+  useEffect(() => {
+    if (state) {
+      onConnections(nodeConnection, state, googleFile)
+    }
+  }, [state])
+
+  // useEffect(() => {
+  //   if (nodeConnection.slackNode.slackAccessToken) {
+  //     fetchBotSlackChannels(
+  //       nodeConnection.slackNode.slackAccessToken,
+  //       setSlackChannels
+  //     )
+  //   }
+  // }, [nodeConnection])
+
+  return (
+    <aside>
+      <Tabs
+        defaultValue="actions"
+        className="h-screen overflow-scroll pb-24"
+      >
+        <TabsList className="bg-transparent">
+          <TabsTrigger value="actions">Actions</TabsTrigger>
+          <TabsTrigger value="settings">Settings</TabsTrigger>
+        </TabsList>
+        <Separator />
+        <TabsContent
+          value="actions"
+          className="flex flex-col gap-4 p-4"
         >
-          <TabsList className="bg-transparent">
-            <TabsTrigger value="actions">Actions</TabsTrigger>
-            <TabsTrigger value="settings">Settings</TabsTrigger>
-          </TabsList>
-          <Separator />
-          <TabsContent
-            value="actions"
-            className="flex flex-col gap-4 p-4"
-          >
-            {Object.entries(EditorCanvasDefaultCardTypes)
-              .filter(
-                ([_, cardType]) =>
-                  (!nodes.length && cardType.type === 'Trigger') ||
-                  (nodes.length && cardType.type === 'Action')
-              )
-              .map(([cardKey, cardValue]) => (
-                <Card
-                  key={cardKey}
-                  draggable
-                  className="w-full cursor-grab border-black bg-neutral-100 dark:border-neutral-700 dark:bg-neutral-900"
-                  onDragStart={(event: any) =>
-                    onDragStart(event, cardKey as EditorCanvasTypes)
-                  }
-                >
-                  <CardHeader className="flex flex-row items-center gap-4 p-4">
-                    <EditorCanvasIconHelper type={cardKey as EditorCanvasTypes} />
-                    <CardTitle className="text-md">
-                      {cardKey}
-                      <CardDescription>{cardValue.description}</CardDescription>
-                    </CardTitle>
-                  </CardHeader>
-                </Card>
-              ))}
-          </TabsContent>
-          <TabsContent
+          {Object.entries(EditorCanvasDefaultCardTypes)
+            .filter(
+              ([_, cardType]) =>
+                (!nodes.length && cardType.type === 'Trigger') ||
+                (nodes.length && cardType.type === 'Action')
+            )
+            .map(([cardKey, cardValue]) => (
+              <Card
+                key={cardKey}
+                draggable
+                className="w-full cursor-grab border-black bg-neutral-100 dark:border-neutral-700 dark:bg-neutral-900"
+                onDragStart={(event) =>
+                  onDragStart(event, cardKey as EditorCanvasTypes)
+                }
+              >
+                <CardHeader className="flex flex-row items-center gap-4 p-4">
+                  <EditorCanvasIconHelper type={cardKey as EditorCanvasTypes} />
+                  <CardTitle className="text-md">
+                    {cardKey}
+                    <CardDescription>{cardValue.description}</CardDescription>
+                  </CardTitle>
+                </CardHeader>
+              </Card>
+            ))}
+        </TabsContent>
+        <TabsContent
           value="settings"
-          className="-mt-6">
+          className="-mt-6"
+        >
           <div className="px-2 py-4 text-center text-xl font-bold">
             {state.editor.selectedNode.data.title}
           </div>
+
           <Accordion type="multiple">
             <AccordionItem
               value="Options"
@@ -105,11 +128,11 @@ const EditorCanvasSidebar = ({ nodes }: Props) => {
                 nodeConnection={nodeConnection}
               />
             </AccordionItem>
-            </Accordion>
-          </TabsContent>
-        </Tabs>
-      </aside>
-    )
-  }
-  
-  export default EditorCanvasSidebar
+          </Accordion>
+        </TabsContent>
+      </Tabs>
+    </aside>
+  )
+}
+
+export default EditorCanvasSidebar
