7975bc72ad29e3bece9e565c6b9d292150642773
vaibhavmannq <vaibhavmann.10@gmail.com>
2025-03-04 19:18:06 +0530

    add functionality to save templates for Discord, Slack, and Notion in workflow connections

--- my-app/src/app/(main)/(pages)/workflows/_actions/workflow-connections.tsx
+++ my-app/src/app/(main)/(pages)/workflows/_actions/workflow-connections.tsx
@@ -1,5 +1,6 @@
 'use server'
 
+import { Option } from "@/src/components/ui/multiple-selector"
 import { db } from "@/src/lib/db"
 import { auth } from "@clerk/nextjs/server"
 
@@ -18,5 +19,105 @@ export const getGoogleListener = async () => {
       })
   
       if (listener) return listener
+    }
+  }
+
+  export const onCreateNodeTemplate = async (
+    content: string,
+    type: string,
+    workflowId: string,
+    channels?: Option[],
+    accessToken?: string,
+    notionDbId?: string
+  ) => {
+    if (type === 'Discord') {
+      const response = await db.workflows.update({
+        where: {
+          id: workflowId,
+        },
+        data: {
+          discordTemplate: content,
+        },
+      })
+  
+      if (response) {
+        return 'Discord template saved'
+      }
+    }
+    if (type === 'Slack') {
+      const response = await db.workflows.update({
+        where: {
+          id: workflowId,
+        },
+        data: {
+          slackTemplate: content,
+          slackAccessToken: accessToken,
+        },
+      })
+  
+      if (response) {
+        const channelList = await db.workflows.findUnique({
+          where: {
+            id: workflowId,
+          },
+          select: {
+            slackChannels: true,
+          },
+        })
+  
+        if (channelList) {
+          //remove duplicates before insert
+          const NonDuplicated = channelList.slackChannels.filter(
+            (channel) => channel !== channels![0].value
+          )
+  
+          NonDuplicated!
+            .map((channel) => channel)
+            .forEach(async (channel) => {
+              await db.workflows.update({
+                where: {
+                  id: workflowId,
+                },
+                data: {
+                  slackChannels: {
+                    push: channel,
+                  },
+                },
+              })
+            })
+  
+          return 'Slack template saved'
+        }
+        channels!
+          .map((channel) => channel.value)
+          .forEach(async (channel) => {
+            await db.workflows.update({
+              where: {
+                id: workflowId,
+              },
+              data: {
+                slackChannels: {
+                  push: channel,
+                },
+              },
+            })
+          })
+        return 'Slack template saved'
+      }
+    }
+  
+    if (type === 'Notion') {
+      const response = await db.workflows.update({
+        where: {
+          id: workflowId,
+        },
+        data: {
+          notionTemplate: content,
+          notionAccessToken: accessToken,
+          notionDbId: notionDbId,
+        },
+      })
+  
+      if (response) return 'Notion template saved'
     }
   }
